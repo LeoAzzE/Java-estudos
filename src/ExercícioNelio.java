5e387eef77b6aa6a97da33977077bcb9558ec5b5
Borges dev <leosull50@hotmail.com>
2022-07-23 21:42:17 -0300

    start java

---
+++ src/ExercícioNelio.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+
+public class ExercícioNelio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		/*Exercício 1
+		
+		int x , y;
+		x = sc.nextInt();
+		y = sc.nextInt();
+		int result = x + y;
+		System.out.println("A soma entre esses 2 números é:" + result);
+		sc.close(); */
+		
+		/*Exercício 2
+		
+		double raio, area, pi;
+		raio = sc.nextDouble();
+		pi = 3.14159;
+		area = pi * Math.pow(raio, 2);
+		System.out.printf("A área desse circulo é: %.4f%n ", area); */
+		
+		/*Exercício 3
+
+		int A, B, C, D, result;
+			A = sc.nextInt();
+			B = sc.nextInt();
+			C = sc.nextInt();
+			D = sc.nextInt();
+			result = A * B - C * D;
+			System.out.println("A diferença entre os numeros é:" + result);
+		*/
+		
+		/* Exercício 4
+		int number;
+		double hours;
+		double value;
+		double total;
+		number = sc.nextInt();
+		hours = sc.nextDouble();
+		value = sc.nextDouble();
+		total = hours * value;
+		System.out.println("NUMBER =" + number);
+		System.out.printf("SALARY = U$ %.2f%n ",total);
+		*/
+		
+		/* Exercício 5
+		int code1 , code2, numberPC1, numberPC2;
+		Double unit1, unit2 ,result;
+		
+		code1 = sc.nextInt();
+		numberPC1 = sc.nextInt();
+		unit1 = sc.nextDouble();
+		
+		code2 = sc.nextInt();
+		numberPC2 = sc.nextInt();
+		unit2 = sc.nextDouble();
+		
+		System.out.println(code1);
+		System.out.println(numberPC1);
+		System.out.println(unit1);
+		System.out.println(code2);
+		System.out.println(numberPC2);
+		System.out.println(unit2);
+		
+		result = numberPC1 * unit1 + numberPC2 * unit2;
+		System.out.printf("VALOR A PAGAR: R$ %.2f%n ", result);
+		*/
+		
+		/* Exercício 6
+		Double n1, n2, n3, triangulo, circulo, trapezio, quadrado, retangulo ,pi, raio;
+		n1 = sc.nextDouble();
+		n2 = sc.nextDouble();
+		n3 = sc.nextDouble();
+		
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(n3);
+		
+		pi = 3.14159;
+		raio = n3;
+		triangulo = (n1*n3)/2;
+		circulo = pi * Math.pow(raio, 2);
+		trapezio = ((n1 + n2) * n3) / 2;
+		quadrado = (n2 * n2);
+		retangulo = (n1 * n2);
+		
+		System.out.printf("TRIANGULO: %.3f%n" , triangulo);
+		System.out.printf("CIRCULO: %.3f%n" , circulo);
+		System.out.printf("TRAPEZIO: %.3f%n" , trapezio);
+		System.out.printf("QUADRADO: %.3f%n" , quadrado);
+		System.out.printf("RETANGULO: %.3f%n" , retangulo);
+		 Exercício 6*/
+	}
+
+}
